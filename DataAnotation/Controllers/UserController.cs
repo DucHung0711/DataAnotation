d0f9b2907e422e547bbf6ce97024d8391938c06f
Duc Hung <Duc Hung@DESKTOP-SC8P53L>
2022-09-06 21:29:06 -0700

    Add project files.

---
+++ DataAnotation/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using DataAnotation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataAnotation.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<UserModel> listUserModel = new List<UserModel>();
+        // GET: User
+        public ActionResult Index(UserModel model)
+        {
+            //if (!ModelState.IsValid) 
+            //{ 
+            //    return View();
+            //}
+            return View(listUserModel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id) 
+        {
+            UserModel model = listUserModel.Find(emp => emp.Id == id);
+            return View(model); 
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                UserModel model = new UserModel();
+                model.Id = Int32.Parse(collection["Id"]);
+                model.Name = collection["Name"];
+                model.Email = collection["Email"];
+                model.Password = collection["Password"];
+                model.ConfirmPassword = collection["ConfirmPassword"];
+                listUserModel.Add(model);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ DataAnotation/Models/UserModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataAnotation.Models
+{
+    public class UserModel
+    {
+        [Display(Name = "So ID")]
+        [Required(ErrorMessage = "Vui long nhap stt")]
+        public int Id { get; set; }
+
+        [Display(Name = "Ten")]
+        [Required(ErrorMessage = "Vui long nhap ten")]
+        [StringLength(50,MinimumLength = 6, ErrorMessage = "Do dai trong khoang tu 6 - 50 ky tu")]
+        public String Name { get; set; }
+
+        [Display(Name = "Mat khau")]
+        [Required(ErrorMessage = "Nhap mat khau")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Do dai trong khoang tu 5 - 50 ky tu")]
+        //[ScaffoldColumn(false)]
+        public String Password { get; set; }
+
+        [Display(Name = "Xac nhan mat khau")]
+        [Required(ErrorMessage = "Nhap mat khau xac nhan")]
+        [Compare("Password", ErrorMessage = "Mat khau khong khop")]
+        [DataType(DataType.Password)]
+        //[ScaffoldColumn(false)]
+        public String ConfirmPassword { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Nhap email")]
+        [DataType(DataType.EmailAddress)]
+        public String Email { get; set; }
+    }
+}
